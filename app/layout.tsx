0d1e589a547e4f132fc90fe5caad8b35f3487099
ratityaayunila-oss <ratitya.ayunila@gmail.com>
2026-02-08 15:51:32 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "ArtistikDesign",
-  description: "Where creativity meets functionality",
+  title: 'ArtistikDesign',
+  description: 'Where creativity meets functionality',
 }
 
 export default function RootLayout({
@@ -16,9 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
